839bc28ceb05f42cc91fd0f4b40c5b6428a78799
Aleksandar Spasojevic <ARSV@simcorp.com>
2016-09-07 15:49:39 +0000

    - refactored 'returns' calculation

--- R/backtest.R
+++ R/backtest.R
@@ -13,12 +13,8 @@ apply_indicators <- function(){
 
 #' @export
 run_backtest <- function( rebalance_config ){
-
   periods <- lapply(.assets, .subset.xts, rebalance_config)
   rebalance <- lapply(lapply(periods, lapply, end), do.call, what = c)
-  returns <- lapply(periods, lapply, function(period) {
-    sweep(period, first(period), MARGIN = 2, FUN = "/") - 1
-  })
 
   apply_indicators()
   positions <- mapply(function(rebalance, indicator) {
@@ -26,20 +22,38 @@ run_backtest <- function( rebalance_config ){
   },
   rebalance[names(.assets)], .indicators[names(.assets)],
   SIMPLIFY = FALSE)
-
+  
+  returns <- lapply(periods, get_returns)
   strategy_returns <- mapply(function(positions, returns) {
     lapply(positions, function(position){
       Map("*", head(position, -1), tail(returns, -1))
     })
   }, positions[names(.assets)], returns[names(.assets)],
   SIMPLIFY = FALSE)
-
+  
   .strategy <<- lapply(strategy_returns, lapply, function(returns){
     Reduce(function(current_period, next_period) {
       rbind.xts(current_period, as.numeric(last(current_period)) * next_period)
     } , Map("+", 1, lapply(returns, na.fill, 0)))
   })
-
+  
   attr(.strategy, "positions") <<- positions
   attr(.strategy, "configuration") <<- match.call()
+}
+
+
+get_returns <- function(periods) {
+  returns <- attr(
+    Reduce(function(current_, next_, ...){
+      sub <- sweep(next_, last(current_), MARGIN = 2, FUN = "/") - 1
+      structure(next_, 
+                returns = append(attr(current_, "returns"), list(sub)))
+    }, periods), 
+    "returns")
+  
+  # first period return calculation
+  append(list(
+    sweep(periods[[1]], first(periods[[1]]), MARGIN = 2, FUN = "/") - 1
+  ), 
+  returns)
 }
