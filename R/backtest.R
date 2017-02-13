63cb016a6aaf4fd3988f52a5a179366b2262cb5e
Aleksandar Spasojevic <aleksandar.spasojevic@outlook.com>
2017-02-13 16:03:12 +0000

    - refactored 'get_returns' to 'calculate_returns' since it does something - a bit code formatting

--- R/backtest.R
+++ R/backtest.R
@@ -16,47 +16,48 @@ run_backtest <- function( rebalance_config ){
   # positions
   periods <- lapply(.assets, .subset.xts, rebalance_config)
   rebalance <- lapply(lapply(periods, lapply, end), do.call, what = c)
-
+  
   apply_indicators()
   positions <- mapply(function(rebalance, indicator) {
     lapply(indicator, "[", rebalance)
   },
   rebalance[names(.assets)], .indicators[names(.assets)],
   SIMPLIFY = FALSE)
-
-  # performance
-  returns <- lapply(periods, get_returns)
+  
+  # calculate returns
+  returns <- lapply(periods, calculate_returns)
+  # strategy returns
   strategy_returns <- mapply(function(positions, returns) {
     lapply(positions, function(position){
       Map("*", head(position, -1), tail(returns, -1))
     })
   }, positions[names(.assets)], returns[names(.assets)],
   SIMPLIFY = FALSE)
-
+  
+  # accumulate returns
   .strategy <<- lapply(strategy_returns, lapply, function(returns){
     Reduce(function(current_period, next_period) {
       rbind.xts(current_period, as.numeric(last(current_period)) * next_period)
     } , Map("+", 1, lapply(returns, na.fill, 0)))
   })
-
+  
   # save
   attr(.strategy, "positions") <<- positions
   attr(.strategy, "configuration") <<- match.call()
 }
 
-# TODO: refactor to 'calculate_returns'
-get_returns <- function(periods) {
+calculate_returns <- function(periods) {
   returns <- attr(
     Reduce(function(current_, next_, ...){
       sub <- sweep(next_, last(current_), MARGIN = 2, FUN = "/") - 1
-      structure(next_,
-                returns = append(attr(current_, "returns"), list(sub)))
+      structure(next_, returns = append(attr(current_, "returns"), list(sub)))
     }, periods),
     "returns")
-
+  
   # first period return calculation
-  append(list(
-    sweep(periods[[1]], first(periods[[1]]), MARGIN = 2, FUN = "/") - 1
-  ),
-  returns)
+  append(
+    list(
+      sweep(periods[[1]], first(periods[[1]]), MARGIN = 2, FUN = "/") - 1
+    ),
+    returns)
 }
